13c23c17460025dd7e35435b025886b1218677be
Kavalorn <kavalorn@ex.ua>
2020-04-13 12:17:11 +0300

    added isSpotted function

--- src/buildings/tower.ts
+++ src/buildings/tower.ts
@@ -1,3 +1,5 @@
+import {isSpotted} from "../utils/utilityFunctions";
+
 export default {
 
   tick: () => {
@@ -21,13 +23,10 @@ export default {
     const towers: any = Game.rooms[myRoomName].find(FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_TOWER}});
 
 
-    if (hostiles.length > 0) {
+    if (isSpotted(hostiles)) {
       (towers as StructureTower[]).forEach(tower => tower.attack(hostiles[0]));
       console.log("ALERT!!!! WE ARE UNDER ATTACK!!!!!");
-    }
-
-
-    if (hostiles.length === 0) {
+    } else {
       for (const name in Game.creeps) {
         // get the creep object
         const creep = Game.creeps[name];
@@ -40,17 +39,24 @@ export default {
       for (const tower of (towers as StructureTower[])){
         if(tower.energy > (tower.energyCapacity * 0.5)){
 
-          const damagedDefences = tower.room
-            .find(FIND_STRUCTURES)
-            .filter(s => s.structureType === STRUCTURE_WALL || s.structureType === STRUCTURE_RAMPART)
+          const structures = tower.room.find(FIND_STRUCTURES);
+          const damagedDefences = structures
+            .filter(s =>
+              s.structureType === STRUCTURE_WALL ||
+              s.structureType === STRUCTURE_RAMPART
+            )
             .sort((first, second) => first.hits - second.hits);
 
-          const damagedStructures = tower.room
-            .find(FIND_MY_STRUCTURES)
-            .sort((first, second) => first.hits - second.hits);
+          const damagedStructures = structures
+            .filter(s =>
+              s.structureType !== STRUCTURE_WALL &&
+              s.structureType !== STRUCTURE_RAMPART &&
+              s.hits !== s.hitsMax
+            )
+            .sort((first, second) => (second.hitsMax - second.hits) - (first.hitsMax - first.hits));
 
-          if (damagedStructures[0]) {tower.repair(damagedStructures[0])}
-          if (damagedDefences[0]) {tower.repair(damagedDefences[0])}
+          if (isSpotted(damagedStructures)) tower.repair(damagedStructures[0]);
+          if (damagedDefences[0] && !isSpotted(damagedStructures)) tower.repair(damagedDefences[0]);
           }
         }
       }
---
+++ src/utils/utilityFunctions.ts
@@ -0,0 +1,5 @@
+const isSpotted = (targets: any[]): boolean => targets.length > 0;
+
+export {
+  isSpotted
+}
